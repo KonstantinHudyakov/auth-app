d4b128674668610e0ac39b5956f54e2a3be7c9b8
KonstantinHudyakov <kostja19994@gmail.com>
2020-10-03 18:03:02 +0300

    Edit UserStorage to store profiles as text

--- src/main/kotlin/me/khudyakov/authapp/UserStorage.kt
+++ src/main/kotlin/me/khudyakov/authapp/UserStorage.kt
@@ -2,10 +2,11 @@ package me.khudyakov.authapp
 
 import me.khudyakov.authapp.entity.UserProfile
 import java.io.*
+import java.nio.charset.StandardCharsets
 
 class UserStorage {
     companion object {
-        const val fileName: String = "users.bin"
+        const val fileName: String = "users.txt"
     }
 
     private val file = File(fileName)
@@ -36,12 +37,16 @@ class UserStorage {
     }
 
     fun save() {
-        val outputStream = ObjectOutputStream(FileOutputStream(file))
-        outputStream.writeInt(users.size)
+        val writer = PrintWriter(OutputStreamWriter(FileOutputStream(file), StandardCharsets.UTF_8))
+        writer.println(users.size)
         for (user in users) {
-            outputStream.writeObject(user)
+            writer.printUser(user)
         }
-        outputStream.close()
+        writer.close()
+    }
+
+    private fun PrintWriter.printUser(user: UserProfile) {
+        println("${user.name} ${user.password} ${user.isBlocked} ${user.isOnPasswordRestrictions}")
     }
 
     private fun load() {
@@ -49,16 +54,36 @@ class UserStorage {
             if (file.length() == 0L) {
                 initializeUsersList()
             } else {
-                val inputStream = ObjectInputStream(FileInputStream(file))
-                users.clear()
-                val size = inputStream.readInt()
+                val reader = BufferedReader(InputStreamReader(FileInputStream(file), StandardCharsets.UTF_8))
+                val size = reader.readLine().toInt()
                 for (i in 0 until size) {
-                    val user = inputStream.readObject()
-                    users.add(user as UserProfile)
+                    users.add(reader.readUser())
                 }
+                reader.close()
             }
         } catch (ex: Exception) {
             ex.printStackTrace()
         }
+    }
+
+    private fun BufferedReader.readUser(): UserProfile {
+        val line = readLine()
+        val properties = line.split(Regex(" +"))
+        if (properties.size == 3) {
+            return UserProfile(
+                name = properties[0],
+                password = "",
+                isBlocked = properties[1].toBoolean(),
+                isOnPasswordRestrictions = properties[2].toBoolean()
+            )
+        } else if (properties.size == 4) {
+            return UserProfile(
+                name = properties[0],
+                password = properties[1],
+                isBlocked = properties[2].toBoolean(),
+                isOnPasswordRestrictions = properties[3].toBoolean()
+            )
+        }
+        throw IllegalStateException()
     }
 }
